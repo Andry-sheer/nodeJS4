ccdd61239f6fa7b7a3f56b08168763915e78470b
Andrew <Andry.sheremetew@gmail.com>
2026-01-03 21:41:20 +0200

    add some routes + add logging method

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3400;
 
+const loggingMiddle = (req, res, next) => {
+  console.log(`method: ${req.method} url: ${req.url}`);
+  next();
+};
+
+app.get("/", loggingMiddle, (req, res, next) => {
+  res.send("Main page");
+  next();
+});
+
+app.get("/test-json", loggingMiddle, (req, res, next) => {
+  res.json({ foo: 3, bar: 3 });
+  next();
+});
+
+app.get("/redirect", loggingMiddle, (req, res, next) => {
+  res.redirect("/test-json");
+  next();
+});
 
 
 
 
-app.listen(PORT, ()=> {
+
+
+
+
+
+
+
+
+
+app.use((req, res) => {
+  res.status(404).send("page not found | 404");
+});
+
+app.listen(PORT, () => {
   console.log(`server running http://localhost:${PORT}`);
-})
+});
